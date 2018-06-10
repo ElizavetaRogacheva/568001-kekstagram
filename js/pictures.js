c581f9d274f285707caec7ab211bd37d56e6d164
Елизавета Рогачева <liza-97-97-97@mail.ru>
2018-06-10 19:25:03 +0500

    создание и отрисовка элементов

--- js/pictures.js
+++ js/pictures.js
@@ -94,5 +94,26 @@ var makeArrayOfPictures = function (numOfPictures) {
 
 var arrayOfPictures = makeArrayOfPictures(25);
 
+var picturesBlock = document.querySelector('.pictures');
+var pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture__link');
+
+//наполнение данными из массива
+var renderPicture = function (pictureObject) {
+  var pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = pictureObject.url;
+  pictureElement.querySelector('.picture__stat--likes').textContent = pictureObject.likes;
+  pictureElement.querySelector('.picture__stat--comments').textContent = pictureObject.comments.length;
+  return pictureElement;
+};
+
+//создание и отрисовка элементов
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < arrayOfPictures.length; i++) {
+  fragment.appendChild(renderPicture(arrayOfPictures[i]));
+}
+picturesBlock.appendChild(fragment);
+
 
 
