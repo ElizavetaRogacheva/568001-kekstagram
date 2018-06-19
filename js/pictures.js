b6b9bb9042590f0261385c5ca8be3822b4dab9d5
Елизавета Рогачева <liza-97-97-97@mail.ru>
2018-06-20 00:25:48 +0500

    Проверка валидности поля хэш-тегов

--- js/pictures.js
+++ js/pictures.js
@@ -220,10 +220,59 @@ var applyEffect = function () {
   }
 };
 
+var hashtagInput = document.querySelector('.text__hashtags');
+var commentInput = document.querySelector('.text__description');
+var hashtagValue = hashtagInput.value.toLowerCase();
+var hashtagArray = hashtagValue.split(' ');
+var checkHashtag = function (array) {
+  for (var i = 0; i < array.length; i++) {
+    for (var j = i + 1; j < array.length; j++) {
+      if (array[i] === array[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+hashtagInput.addEventListener('invalid', function (evt) {
+  if (!checkHashtag(hashtagArray)) {
+    hashtagInput.setCustomValidity('Присутствуют одинаковые хэш-теги!');
+  }
+  for (var i = 0; i < hashtagArray.length; i++) {
+
+  }
+});
+
+
+
+//alert(hashtagValue);
+/*userNameInput.addEventListener('invalid', function (evt) {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  }
+});*/
+
 drawElements();
 hideBlocks();
 openAndCloseUploadBlock();
 createSizeButtonsActions();
 applyEffect();
+
+
+
+
+
+
+
+
+
+
+
+
 
 
