d82a82e6a76ddc55699c87b7d86cee649948309f
Елизавета Рогачева <liza-97-97-97@mail.ru>
2018-07-04 18:46:52 +0500

    Переотрисовка фото при нажатии кнопок фильтров

--- js/picture.js
+++ js/picture.js
@@ -2,12 +2,13 @@
 (function () {
   var NEW_PHOTOS_AMOUNT = 10;
   var photos = [];
-  var currentPhotos = [];
+  var currentButton = null;
   var picturesBlock = document.querySelector('.pictures');
   var pictureTemplate = document.querySelector('#picture')
     .content
     .querySelector('.picture__link');
   var imgFilters = document.querySelector('.img-filters');
+  var fragment = document.createDocumentFragment();
 
   var filterButtons = {
     popular: document.querySelector('#filter-popular'),
@@ -20,17 +21,23 @@
   };
 
   var drawPhotos = function (elem) {
-    var fragment = document.createDocumentFragment();
-    for (var j = 0; j < elem.length; j++) {
-      fragment.appendChild(renderPicture(elem[j]));
-    }
+    elem.forEach(function (element) {
+      fragment.appendChild(renderPicture(element));
+    });
     picturesBlock.appendChild(fragment);
   };
 
+  var removePhotos = function () {
+    picturesBlock.querySelectorAll('.picture__link').forEach(function (element) {
+      picturesBlock.removeChild(element);
+    });
+  };
+
   var onLoadDataFromServer = function (object) {
-    photos = object;
+    photos = object.slice(0, object.length);
     drawPhotos(object);
     imgFilters.classList.remove('img-filters--inactive');
+    currentButton = filterButtons.popular;
   };
 
   var onErrorDataFromServer = function (object) {
@@ -40,26 +47,38 @@
 
   // обработчики
   var popularButtonClickHandler = function () {
+    currentButton.classList.remove('img-filters__button--active');
+    currentButton = filterButtons.popular;
+    currentButton.classList.add('img-filters__button--active');
+    removePhotos();
     drawPhotos(photos);
   };
 
   var newButtonClickHandler = function () {
+    currentButton.classList.remove('img-filters__button--active');
+    currentButton = filterButtons.new;
+    currentButton.classList.add('img-filters__button--active');
+    removePhotos();
+    var copysOfPhotos = photos.slice(0, photos.length);
     var newPhotos = [];
     for (var i = 0; i < NEW_PHOTOS_AMOUNT; i++) {
-      var index = window.data.getRandomIndex(0, photos.length - 1);
-      newPhotos.push(photos[index]);
-      photos.splice(index, 1);
+      var index = window.data.getRandomIndex(0, copysOfPhotos.length - 1);
+      newPhotos.push(copysOfPhotos[index]);
+      copysOfPhotos.splice(index, 1);
     }
-    console.log(newPhotos);
     drawPhotos(newPhotos);
   };
 
   var discussedButtonClickHandler = function () {
-    photos.sort(function (a, b) {
+    currentButton.classList.remove('img-filters__button--active');
+    currentButton = filterButtons.discussed;
+    currentButton.classList.add('img-filters__button--active');
+    removePhotos();
+    var copysOfPhotos = photos.slice(0, photos.length);
+    copysOfPhotos.sort(function (a, b) {
       return b.comments.length - a.comments.length;
     });
-    drawPhotos(photos);
-    console.log(photos);
+    drawPhotos(copysOfPhotos);
   };
 
   // обработка событий
